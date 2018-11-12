9f4c1f76601044dd7ea4fe811de8ab6802bb8089
unknown <p.wolski1000@gmail.com>
2018-11-12 13:30:40 +0100

    fixes some css

--- src/components/MainText.js
+++ src/components/MainText.js
@@ -8,13 +8,17 @@ const Wrapper = styled.div`
     color: #707070;
     margin: 0 auto;
     p {
+        text-align: justify;
+        text-justify: inter-word;
         width: 80%;
-        /* text-align: center; */
         margin: 0 auto;
+        text-decoration: justify;
         text-indent: 50px;
         word-wrap: break-word;
         margin-top: 50px;
         margin-bottom: 50px;
+        font-size: 2.7rem;
+        line-height: 2.9rem;
         
     }
 `
