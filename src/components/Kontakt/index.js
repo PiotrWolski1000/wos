f219b5fe5f1abacd81029194c96bbdda9d06a569
piotr Wolski <p.wolski1000@gmail.com>
2019-01-09 22:36:51 +0100

    updating form

--- src/components/Kontakt/index.js
+++ src/components/Kontakt/index.js
@@ -1,27 +1,173 @@
-import React from 'react'
 import * as El from './style'
-const KontaktForm = () => (
-  <El.Wrapper>
-    <El.ContactForm>
-        <form 
-            name="contact"
-            method="post"
-            // data-netlify="true"
-            // data-netlify-honeypot="bot-field"
-        >
 
-            <input name="name" type='text' placeholder="Name, Company name"/>
+import React, { Component } from 'react';
 
-            <input name="email" type="email" placeholder="Email address" />
-            <input name="phone" type="tel" placeholder="Phone number"/>
-            <input name="mail_subject" type="text" placeholder="Subject/Title"/>
-            <textarea name="message" type="text" rows="4" cols="50" placeholder="Message" />
-            <El.Button>
-                Nachricht senden
-            </El.Button>
-        </form>
-    </El.ContactForm>
-  </El.Wrapper>
-)
+class KontaktForm extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            nameFieldActive: false,
+            name: '',
+            emailFieldActive: false,
+            email: '',
+            phoneFieldActive: false,
+            phone: '',
+            subjectFieldActive: false,
+            subject: '',
+            messageFieldActive: false,
+            message: ''
 
+        }
+    }
+
+    onChangeInput = e => {
+        const { name, value } = e.target
+        this.setState(prevState => ({ ...prevState, [name]: value }))
+    
+        this.activateField(e);
+        e.preventDefault();
+      }
+    
+      activateField = (e) => {
+        if(e.target.name === 'name'){
+          this.setState({
+            nameFieldActive: true
+          })
+        }else if(e.target.name === 'email'){
+          this.setState({
+              emailFieldActive: true
+          })
+        }else if(e.target.name === 'phone'){
+            this.setState({
+                phoneFieldActive: true
+            })
+        }else if(e.target.name === 'subject'){
+            this.setState({
+                subjectFieldActive: true
+            })
+        }else if(e.target.name === 'message'){
+            this.setState({
+                messageFieldActive: true
+            })
+        }
+      }
+    
+      disableFocus = e => {
+        if(e.target.name === 'name'){
+          if (e.target.value === '') {
+            this.setState({
+                nameFieldActive: false
+            })
+          }
+        }else if(e.target.name === 'email'){
+          if (e.target.value === '') {
+            this.setState({
+                emailFieldActive: false
+            })
+          }
+        } 
+        else if(e.target.name === 'phone'){
+            if (e.target.value === '') {
+              this.setState({
+                  phoneFieldActive: false
+              })
+            }
+        }else if(e.target.name === 'subject'){
+            if (e.target.value === '') {
+              this.setState({
+                  subjectFieldActive: false
+              })
+            }
+        }else if(e.target.name === 'message'){
+            if (e.target.value === '') {
+              this.setState({
+                  messageFieldActive: false
+              })
+            }
+        } 
+
+      }
+
+    render() {
+        return (
+            <El.Wrapper>
+                    <El.Form 
+                
+                        name="contact"
+                        method="post"
+                        data-netlify="true"
+                        data-netlify-honeypot="bot-field"
+                    >
+                    <h3 style={{color: '#363636'}}>
+                        Kontaktformular
+                    </h3>
+                        <label htmlFor='name' className={this.state.nameFieldActive ? "field-active" : ""}>Name</label>
+                        <input 
+                            name='name' 
+                            type='text' 
+                            value={this.state.name}
+                            placeholder="Name, Firma Name"
+                            onFocus={this.activateField}
+                            onBlur={this.disableFocus}   
+                            onChange={this.onChangeInput}
+                            required
+                        />
+            
+                        <label htmlFor='email' className={this.state.emailFieldActive ? "field-active" : ""}>E-mail</label>
+                        <input 
+                            name='email' 
+                            type="email" 
+                            value={this.state.email}
+                            placeholder="E-mail addresse" 
+                            onFocus={this.activateField}
+                            onBlur={this.disableFocus}   
+                            onChange={this.onChangeInput}
+                            required
+                        />
+                        
+                        <label htmlFor='phone' className={this.state.phoneFieldActive ? "field-active" : ""}>Telefonnummer</label>
+                        <input 
+                            name="phone" 
+                            type="tel" 
+                            value={this.state.phone}
+                            placeholder="Telefonnnummer"
+                            onFocus={this.activateField}
+                            onBlur={this.disableFocus}   
+                            onChange={this.onChangeInput}
+                            required
+                        />
+
+                        <label htmlFor='subject' className={this.state.subjectFieldActive ? "field-active" : ""}>Thema</label>            
+                        <input 
+                            name="subject" 
+                            type="text"
+                            value={this.state.subject}
+                            placeholder="Thema"
+                            onFocus={this.activateField}
+                            onBlur={this.disableFocus}   
+                            onChange={this.onChangeInput}
+                            required
+                        />
+
+                        <label htmlFor='message' className={this.state.messageFieldActive ? "field-active" : ""}>Nachricht</label>
+                        <textarea
+                            value={this.state.message}
+                            name="message" 
+                            type="text" 
+                            rows="4" 
+                            cols="50" 
+                            placeholder="Message" 
+                            onFocus={this.activateField}
+                            onBlur={this.disableFocus}   
+                            onChange={this.onChangeInput}
+                            required
+                        />
+                        <El.Button>
+                            Nachricht senden
+                        </El.Button>
+                    </El.Form>
+            </El.Wrapper>
+        );
+    }
+}
 export default KontaktForm
--- src/components/Kontakt/style.js
+++ src/components/Kontakt/style.js
@@ -1,38 +1,78 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.div`
-    display: block;
-    height: auto;
-    min-height: 300px;
-    width: 100%;
-
-    color: #707070;
-`
-
-export const ContactForm = styled.form`
     display: flex;
     flex-direction: column;
 
     justify-content: center;
+
     align-items: center;
+    height: auto;
+    min-height: 300px;
+    width: 100%;
+    z-index: 0;
+    color: #707070;
 
-    input, textarea {
-        padding:0.5rem;
-        display: block;
+`
+
+export const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    margin-top: 5rem;
+    @media(min-width: 320px){
+        width: 250px;
+    }
+    @media(min-width: 425px){
+        width: 350px;
+    }
+    @media(min-width: 768px){
         width: 500px;
-       
-        border-radius: 10px;
-
-        margin-top: 1rem;
-        margin-bottom: 1rem;
+    }
+    @media(min-width: 1024px){
+        width: 768px;
     }
 
-    @media(max-width: 768px) {
-        input, textarea{
-            width: 300px;
+    textarea[type='text'], input[type='tel'], input[type='text'],input[type='password'],input[type='email'] {
+        all: initial;
+        background-color: white;
+        padding: 0.3rem 0.2rem;
+        border-radius: 3px;
+        z-index: 2;
+        outline: none;
+        border-bottom: 2px solid #E52321;;
+        &::-webkit-input-placeholder {
+            color: grey;
         }
     }
+    label {
+        z-index: 1;
+        display: inline-block;
+        display: block;
+        position:relative;
+        left: 2px;
+        bottom: -29px;
+        transition: all 150ms ease-in;
+        /* color: #ffffff; */
+        color: black;
+        cursor: default;
+    }
+    label.field-active {
+        transform: translateY(-30px);
+        font-size: .8rem;
+    }
+    .floating-label {
+        -webkit-appearance: none !important;
+    }
+    textarea:focus::-webkit-input-placeholder,input:focus::-webkit-input-placeholder{
+        opacity: 0;
+        transition: opacity 0.5s ease; 
+    }
+    textarea:not(:focus)::-webkit-input-placeholder,input:not(:focus)::-webkit-input-placeholder{
+        opacity: 1;
+        transition: opacity 0.5s ease; 
+    }
 `
+
 
 export const Button = styled.button`
     all: initial;
@@ -70,4 +110,19 @@ export const Button = styled.button`
         min-width: 400px;
     }
 
+    -webkit-box-shadow: 0px 3px 6px 0px rgba(0,0,0,0.2);
+    -moz-box-shadow: 0px 3px 6px 0px rgba(0,0,0,0.2);
+    box-shadow: 0px 3px 6px 0px rgba(0,0,0,0.2);
+    outline: none;
+
+    &:hover{
+        text-decoration: underline;
+    }
+
+    ${this}:active{
+        background-color: #CE1715;
+        color: white;
+        text-decoration: none !important;
+        transition: all 0.25s ease;
+    }
 `
