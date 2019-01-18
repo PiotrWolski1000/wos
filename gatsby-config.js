a87d3c7ac599c9edf8fe95bfdec84a7345d164f3
piotr Wolski <p.wolski1000@gmail.com>
2019-01-18 11:16:39 +0100

    updating theme color of browser mobile gatsbyconfig

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,8 +19,8 @@ module.exports = {
         name: 'gatsby-starter-default',
         short_name: 'starter',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#E52321',
+        theme_color: '#E52321',
         display: 'minimal-ui',
         icon: 'src/images/headerImage.png', // This path is relative to the root of the site.
       },
