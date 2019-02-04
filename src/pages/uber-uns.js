2e21189ec3563559f22238ad9078272dc775d2de
piotr Wolski <p.wolski1000@gmail.com>
2019-02-04 01:44:46 +0100

    adding new about us component

--- src/pages/uber-uns.js
+++ src/pages/uber-uns.js
@@ -1,12 +1,13 @@
 import React from 'react'
 // import { Link } from 'gatsby'
-
+import Hausmeister from '../components/Hausmeister/'
 import Layout from '../components/layout'
 import {graphql} from 'gatsby'
 import SimpleSlider from '../components/SimpleSlider'
 
 const UberUns = (data) => (
   <Layout>
+    <Hausmeister/>
     <SimpleSlider></SimpleSlider>
   </Layout>
 )
