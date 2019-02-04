3041c04f997945ccd5519b3c4be422ccb02f9429
piotr Wolski <p.wolski1000@gmail.com>
2019-02-04 01:43:25 +0100

    updates styles of wrapper element

--- src/components/MainText.js
+++ src/components/MainText.js
@@ -1,31 +1,37 @@
 import React from 'react'
 import styled from 'styled-components'
+import Link from 'gatsby-link'
 
 const Wrapper = styled.div`
-    display: block;
+    display: block; 
     height: auto;
     width: 100%;
-    color: #707070;
-    margin: 0 auto;
+    /* color: #707070; */
+    margin: 2rem 0 0.5rem 0;
+    font-size: 1.4rem;
+    padding: 0rem 1rem 0rem 1rem;
+    font-style: italic;
+    /* color: rgba(0,0,0,0.8); */
     p {
-        font-family: Segoe UI;
-        text-align: justify;
-        text-justify: inter-word;
-        width: 80%;
-        margin: 0 auto;
-        text-indent: 1em;
-        /* word-wrap: break-word; */
-        margin-top: 1em;
-        margin-bottom: 1em;
-        font-size: 1.4rem;
-        line-height: 1.4em;
+        text-align: left;
+        color: #707070;
+        text-justify: left;
+        text-indent: 2rem;
+
     }
+
 `
 const MainText = () => (
   <Wrapper>
-    <p>   
-        Hier finden Sie alle Informationen über unsere Dienstleistungen. 
-    </p>
+        <p>
+        Hier finden Sie alle Informationen über unsere Dienstleistungen, die Sie
+        {" "} 
+        <Link to='/leistungen'>
+            hier 
+        </Link>
+        {" "}
+        überprüfen können.
+        </p>
   </Wrapper>
 )
 
