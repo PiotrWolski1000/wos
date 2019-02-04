2c55a62f3f5679167d8001ea7578250eca8d1188
piotr Wolski <p.wolski1000@gmail.com>
2019-02-04 01:44:30 +0100

    testing display new component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,39 @@
 import React from 'react'
 // import { Link } from 'gatsby'
-
 import Layout from '../components/layout'
 import MainImage from '../components/MainImage'
 import MainText from '../components/MainText'
 import {graphql} from 'gatsby'
 import Insel from '../components/Insel'
 import SimpleSlider from '../components/SimpleSlider'
+import Hausmeister from '../components/Hausmeister/'
+import Link from 'gatsby-link'
+import Test from './../components/Test/'
 
-const IndexPage = (data) => (
+const IndexPage = ({data}) => (
   <Layout>
     <MainImage></MainImage>
     <MainText></MainText>
+    <Test></Test>
+    {/* {console.log('my data: ',{data})}
+    {console.log(data.imageHausmeister.childImageSharp.fluid)} */}
+    {/* <Hausmeister image={data}/> */}
     <Insel/>
      <div style={{width: '100%'}}>
       <SimpleSlider/>
-
     </div>
   </Layout>
 )
 
 export const indexQuery = graphql`
-  query IndexQuery {
-    file(relativePath: { eq: "headerImage.png" }) {
-      childImageSharp {
-        fixed(width: 125, height: 125) {
-          ...GatsbyImageSharpFixed
-        }
+query IndexQuery {
+  imageHausmeister: file(relativePath: { eq: "hausmeister.jpg" }) {
+    childImageSharp {
+      fluid(maxWidth: 500) {
+        ...GatsbyImageSharpFluid
       }
     }
-  }
+  }  
+}
 `
-
 export default IndexPage
