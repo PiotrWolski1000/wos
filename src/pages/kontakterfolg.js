744dd794c18e3ac10a3cf8c5df6db4fda4169efb
piotr Wolski <p.wolski1000@gmail.com>
2019-02-09 01:02:50 +0100

    adding simple slider and flex style to page content

--- src/pages/kontakterfolg.js
+++ src/pages/kontakterfolg.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import Layout from '../components/layout'
-
+import SimpleSlider from '../components/SimpleSlider'
 class kontakterfolg extends Component {
     render() {
         return (
             <Layout>
-                <div style={{height: '768px'}}>
-                    <p style={{fontSize: '1.3rem', textAlign: 'center'}}>
+                <div style={{height: '768px' ,display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
+                    <p style={{display: 'block', width: '80%', fontSize: '1.3rem', margin: '0 auto', textAlign: 'center'}}>
                         Vielen Dank für Ihre Kontaktaufnahme, wir werden uns in Kürze mit Ihnen in Verbindung setzen.
                     </p>
+                    <SimpleSlider/>
                 </div>
             </Layout>
         );
