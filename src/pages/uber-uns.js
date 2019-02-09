d627f03dcc5e8ecd288adedb16b448b765ba5ac9
piotr Wolski <p.wolski1000@gmail.com>
2019-02-09 20:08:45 +0100

    adding meta data

--- src/pages/uber-uns.js
+++ src/pages/uber-uns.js
@@ -4,9 +4,13 @@ import Hausmeister from '../components/Hausmeister/'
 import Layout from '../components/layout'
 import {graphql} from 'gatsby'
 import SimpleSlider from '../components/SimpleSlider'
-
+import Helmet from 'react-helmet'
 const UberUns = (data) => (
   <Layout>
+    <Helmet>
+      <title>Über uns</title>
+      <meta name="description" content="WoS Hausmeister & Reinigung Service auf Föhr. Wir sind neu auf Föhr Insel. Wir bieten Dienstleistungen wie Reinigungsdienst, Gartenpflege, Hausarbeit und mehr. Kontakt uns für weitere Informationen." />
+    </Helmet>
     <Hausmeister/>
     <SimpleSlider></SimpleSlider>
   </Layout>
